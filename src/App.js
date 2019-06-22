ba2ce26afb1b015975314133b05967c5d017595d
Lucas <lucasrafael431@gmail.com>
2019-06-22 16:56:49 -0300

    using toast

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
 import { GlobalStyle } from './styles/global';
 import Main from './components/Main';
 import store from './store';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => (
   <Provider store={store}>
     <GlobalStyle />
     <Main />
+    <ToastContainer autoclose={5000} />
   </Provider>
 );
 
--- src/store/sagas/tools.js
+++ src/store/sagas/tools.js
@@ -1,4 +1,5 @@
 import { call, put, select } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 import api from '../../services/api';
 import { Creators as ToolsCreators } from '../ducks/tools';
 
@@ -19,7 +20,9 @@ export function* getTools(action) {
       yield put(ToolsCreators.getToolsSuccess(data));
     }
   } catch (err) {
-    console.log(err);
+    toast.error('Error fetching tools', {
+      position: toast.POSITION.TOP_RIGHT,
+    });
   }
 }
 
@@ -28,18 +31,20 @@ export function* addTool(action) {
   const isDuplicated = yield select(state => state.tools.find(tool => tool.title === data.title));
 
   if (isDuplicated) {
-    /**
-     * apply toast
-     */
+    toast.error('Tool already registered', {
+      position: toast.POSITION.TOP_RIGHT,
+    });
   } else {
     try {
       const response = yield call(api.post, '/tools', data);
-      /**
-       * apply toast
-       */
+      toast.success('Tool successfully added', {
+        position: toast.POSITION.TOP_RIGHT,
+      });
       yield put(ToolsCreators.addToolSuccess(response.data));
     } catch (err) {
-      console.log(err);
+      toast.error('Error adding tool', {
+        position: toast.POSITION.TOP_RIGHT,
+      });
     }
   }
 }
@@ -48,11 +53,13 @@ export function* removeTool(action) {
   const { id } = action.payload;
   try {
     yield call(api.delete, `/tools/${id}`);
-    /**
-     * apply toast
-     */
+    toast.success('Tool successfully removed', {
+      position: toast.POSITION.TOP_RIGHT,
+    });
     yield put(ToolsCreators.getTools());
   } catch (err) {
-    console.log(err);
+    toast.error('Error removing tool', {
+      position: toast.POSITION.TOP_RIGHT,
+    });
   }
 }
